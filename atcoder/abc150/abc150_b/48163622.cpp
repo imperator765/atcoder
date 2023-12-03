74c6301aa8e74771b6888cad3cfe41d5ebdcd0cb
imperator765 <colonel2410@gmail.com>
2023-12-03 14:34:26 +0900

    abc150_b

---
+++ atcoder/abc150/abc150_b/48163622.cpp
@@ -0,0 +1,20 @@
+#include<bits/stdc++.h>
+using namespace std;
+
+int main() {
+    int N;
+    string S;
+    cin >> N >> S;
+
+    int count = 0;
+
+    for(int i = 0; i < N - 2; i++){
+        if(S.at(i) == 'A' && S.at(i + 1) == 'B' && S.at(i + 2) == 'C'){
+            count++;
+        }
+    }
+
+    cout << count << endl;
+
+    return 0;
+}
