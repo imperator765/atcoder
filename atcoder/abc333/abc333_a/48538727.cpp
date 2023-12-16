8f1c14fc16c0a9891192ff76cadd8b0518efbf15
imperator765 <colonel2410@gmail.com>
2023-12-16 21:02:56 +0900

    abc333_a

---
+++ atcoder/abc333/abc333_a/48538727.cpp
@@ -0,0 +1,13 @@
+#include<bits/stdc++.h>
+using namespace std;
+
+int main() {
+    int N;
+    cin >> N;
+    string ans;
+    for(int i=0; i<N; i++){
+        ans+=to_string(N);
+    }
+    cout << ans << endl;
+    return 0;
+}
