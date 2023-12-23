362e8978d67405de72ddb5f11f3947b73f16747e
imperator765 <colonel2410@gmail.com>
2023-12-23 21:02:31 +0900

    abc334_a

---
+++ atcoder/abc334/abc334_a/48738243.cpp
@@ -0,0 +1,15 @@
+#include<bits/stdc++.h>
+using namespace std;
+
+int main() {
+    int B, G;
+    cin >> B >> G;
+
+    if(B>G){
+        cout << "Bat" << endl;
+    }else{
+        cout << "Glove" << endl;
+    }
+
+    return 0;
+}
