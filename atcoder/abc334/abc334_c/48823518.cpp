8751a618a81345479597addf931b2a9f01d4a17d
imperator765 <colonel2410@gmail.com>
2023-12-24 19:04:27 +0900

    abc334_c

---
+++ atcoder/abc334/abc334_c/48823518.cpp
@@ -0,0 +1,42 @@
+#include<bits/stdc++.h>
+using namespace std;
+
+int main() {
+    long N, K;
+    cin >> N >> K;
+
+    vector<long> A(K);
+    for (long i = 0; i < K; ++i) {
+        cin >> A[i];
+    }
+    long ans=0;
+
+    if((2*N-K)%2==0){
+        for(long i=0; i<K; i++){
+            if(i%2==1){
+                ans+=A[i]-A[i-1];
+            }
+        }
+    }else{
+        vector<long> B(K+1);
+        vector<long> E(K+1);
+        for(long i=1; i<=K; i++){
+            B[i]=B[i-1];
+            if(i%2==0){
+                B[i]+=A[i-1]-A[i-2];
+            }
+        }
+        for(long i=K-1; i>=0; i--){
+            E[i]=E[i+1];
+            if((K-i)%2==0){
+                E[i]+=A[i+1]-A[i];
+            }
+        }
+        ans = 2*N;
+        for(long i=0; i<K; i++){
+            ans=min(ans, B[i]+E[i]);
+        }
+    }
+    cout << ans << endl;
+    return 0;
+}
