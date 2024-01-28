6d1219d8a88d337b24a3106145d030c592cca276
imperator765 <colonel2410@gmail.com>
2024-01-28 20:20:00 +0900

    abc338_d

---
+++ atcoder/abc338/abc338_d/49766750.cpp
@@ -0,0 +1,26 @@
+#include<bits/stdc++.h>
+using namespace std;
+
+int main() {
+    int N, M;
+    cin >> N >> M;
+    vector<int> X(M);
+    for (int i = 0; i < M; i++) cin >> X[i];
+    vector<long long> v(N + 1);
+    for (int j = 0; j < M - 1; j++) {
+        int small = min(X[j], X[j + 1]);
+        int large = max(X[j], X[j + 1]);
+        v[small] += N - (large - small);
+        v[large] += -(N - (large - small));
+        v[1] += large - small;
+        v[small] += -(large - small);
+        v[large] += large - small;
+    }
+    long long distance = LLONG_MAX;
+    for(int i = 1; i < N + 1 ; i++){
+        v[i] += v[i - 1];
+        distance = min(v[i], distance);
+    }
+    cout << distance << endl;
+    return 0;
+}
