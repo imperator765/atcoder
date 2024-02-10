1fd7173b762a72d7b3c27400613ec42162043a46
imperator765 <colonel2410@gmail.com>
2024-02-10 21:06:41 +0900

    abc340_b

---
+++ atcoder/abc340/abc340_b/50149184.cpp
@@ -0,0 +1,26 @@
+#include<bits/stdc++.h>
+using namespace std;
+
+int main() {
+    int Q;
+    cin >> Q;
+    vector<int> A;
+
+    for(int i = 0; i < Q; ++i) {
+        int query_type;
+        cin >> query_type;
+
+        if(query_type == 1) {
+            int x;
+            cin >> x;
+            A.push_back(x);
+        }
+        else if(query_type == 2) {
+            int k;
+            cin >> k;
+            cout << A[A.size() - k] << endl;
+        }
+    }
+
+    return 0;
+}
